eb55f06da1111e6753311df22192f75299e36864
cuxovengeance <tiare.veliza@mayor.cl>
2020-01-24 18:34:42 -0300

    Modificacion de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,29 @@ import { NgModule } from '@angular/core';
 /*RUTAS*/
 import {APP_ROUTES} from './app.routes';
 
+/*MODULOS*/
+import { PageModule } from './pages/pages.module';
+
+/*COMPONENTES*/
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
-import {RouterModule} from '@angular/router';
-import {DashboardComponent} from './pages/dashboard/dashboard.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+
+import {RouterModule} from '@angular/router';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumsComponent,
-    DashboardComponent,
-    PagesComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
